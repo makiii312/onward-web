7d76b454b62e02dd7675c576bdd78c943aeb8dbf
Jeremay Micah Vallado <jeremaymicah@gmail.com>
2026-04-03 17:10:25 +0800

    refactor: adjust auth layout and footer element

--- src/pages/Auth/AuthLayout.tsx
+++ src/pages/Auth/AuthLayout.tsx
@@ -3,13 +3,13 @@ import './Auth.css';
 
 const AuthLayout = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start">
-      <div className="mt-10 flex w-full max-w-md flex-col items-center gap-10">
+    <div className="flex min-h-svh flex-col items-center justify-start">
+      <div className="mt-10 flex w-full max-w-md grow flex-col items-center gap-10">
         <img src="/src/assets/onward-logo.svg" alt="Onward Logo" />
         <Outlet />
       </div>
 
-      <footer className="absolute bottom-4 w-full text-center text-sm text-gray-500">
+      <footer className="relative my-10 w-full justify-self-end text-center text-sm text-gray-500">
         &copy; 2026 Onward
       </footer>
     </div>
