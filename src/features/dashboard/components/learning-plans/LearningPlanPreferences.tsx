9892c57413b48ed393a09a7b70a5db23eeaad5a0
Jeremay Micah Vallado <jeremaymicah@gmail.com>
2026-05-01 00:01:09 +0800

    feat: initual ui structure for learning plans stage

---
+++ src/features/dashboard/components/learning-plans/LearningPlanPreferences.tsx
@@ -0,0 +1,9 @@
+const LearningPlanPreferences = () => {
+  return (
+    <section className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-4">
+      <h2>Learning Plan Preferences</h2>
+    </section>
+  );
+};
+
+export default LearningPlanPreferences;
--- src/features/dashboard/pages/LearningPlans.tsx
+++ src/features/dashboard/pages/LearningPlans.tsx
@@ -1,8 +1,39 @@
+import { Separator } from '@/shared/components/ui/separator';
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from '@/shared/components/ui/tabs';
+import LearningPlanList from '../components/learning-plans/LearningPlanList';
+import LearningPlanPreferences from '../components/learning-plans/LearningPlanPreferences';
+
 const LearningPlansPage = () => {
   return (
-    <>
-      <h1>LearningPlansPage</h1>
-    </>
+    <div className="flex flex-col gap-8">
+      <div className="flex items-center justify-between px-8 pt-10">
+        {/* Page Header */}
+        <h1 className="text-2xl font-bold text-purple-700">Learning Plans</h1>
+      </div>
+
+      <Tabs defaultValue="plans">
+        <TabsList className="px-8" variant="line">
+          <TabsTrigger value="plans">Plans</TabsTrigger>
+          <TabsTrigger value="preferences">Preferences</TabsTrigger>
+        </TabsList>
+        <Separator />
+
+        <div className="flex flex-col gap-8 pt-6">
+          <TabsContent value="plans" className="px-8">
+            <LearningPlanList />
+          </TabsContent>
+
+          <TabsContent value="preferences" className="px-8">
+            <LearningPlanPreferences />
+          </TabsContent>
+        </div>
+      </Tabs>
+    </div>
   );
 };
 
